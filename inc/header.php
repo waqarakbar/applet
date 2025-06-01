e2d7af8931a296e6c1c7ad84279284420a9edcac
Waqar Akbar <waqarakbar89@gmail.com>
2025-06-01 12:28:26 +0500

    title

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap Page with Menu</title>
+  <title>GHL Applet</title>
   <!-- Bootstrap CSS -->
   <link href="./assets/bootstrap-5.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
